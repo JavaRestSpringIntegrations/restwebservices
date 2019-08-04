4030b610721433e57995bd73e8570caf78e8f1ef
Sunil.Jacob <sunil1405@gmail.com>
2019-08-04 15:55:35 +0530

    Used I18N

--- src/main/java/com/rest/webservices/RestfulWebServicesApplication.java
+++ src/main/java/com/rest/webservices/RestfulWebServicesApplication.java
@@ -2,6 +2,11 @@ package com.rest.webservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
@@ -10,4 +15,17 @@ public class RestfulWebServicesApplication {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 	}
 
+	@Bean
+	public SessionLocaleResolver localResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
+	}
 }
--- src/main/java/com/rest/webservices/helloworld/HelloWorldController.java
+++ src/main/java/com/rest/webservices/helloworld/HelloWorldController.java
@@ -1,11 +1,19 @@
 package com.rest.webservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     // == methods ==
     @GetMapping("/hello")
@@ -26,6 +34,11 @@ public class HelloWorldController {
         if (name == String.format("test")) // have to tweak this part
             throw new NameInvalidException("name-"+ name);
         return new HelloWorld(String.format("Hello Word, %s",name));
+    }
+
+    @GetMapping("/greeting")
+    public String greetingInternationalized(@RequestHeader(name = "Accept-Language",required = false) Locale locale) {
+        return messageSource.getMessage("good.morning.message",null,locale);
     }
 
 }
