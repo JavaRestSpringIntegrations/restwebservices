7411f71c8e34cb0b69a78c6f425fd42ac5383924
Sunil.Jacob <sunil1405@gmail.com>
2019-08-20 14:57:19 +0530

    fix

--- src/main/java/com/rest/webservices/RestfulWebServicesApplication.java
+++ src/main/java/com/rest/webservices/RestfulWebServicesApplication.java
@@ -3,7 +3,6 @@ package com.rest.webservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 import java.util.Locale;
